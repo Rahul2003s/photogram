985c313850e7de70e4af8cb49a29bf8e42aecae5
Rahul S <rahulsri@gmail.com>
2023-05-17 11:55:11 +0530

    added css file for each file and added a function in _head to load css dynamically from the css folder

---
+++ _templates/_head.php
@@ -0,0 +1,27 @@
+<head>
+	<script src="assets/js/color-modes.js"></script>
+
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="">
+	<meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+	<meta name="generator" content="Hugo 0.111.3">
+	<title>Album example · Bootstrap v5.3</title>
+
+	<link rel="canonical" href="https://getbootstrap.com/docs/5.3/examples/album/">
+
+
+
+
+
+	<link href="assets/dist/css/bootstrap.min.css" rel="stylesheet">
+	
+	<?php if (file_exists($_SERVER['DOCUMENT_ROOT'].'/photogram/assets/dist/css/'.basename($_SERVER['SCRIPT_NAME'],".php").'.css')) {?>
+		<link href="assets/dist/css/<?php echo basename($_SERVER['SCRIPT_NAME'],".php"); ?>.css" rel="stylesheet">
+	<?php
+	}?>
+
+	
+
+
+</head>
